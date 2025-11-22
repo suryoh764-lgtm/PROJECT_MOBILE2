92caeb4cf9cfaa7b7ce4925ec12718de5e8a88d7
YO YO <suryoh764@github.com>
2025-11-22 15:20:29 +0700

    Pembuatan menuscreen dasar

--- Yasaka/screens/user/MenuScreen.tsx
+++ Yasaka/screens/user/MenuScreen.tsx
@@ -1,10 +1,50 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import { BlurView } from 'expo-blur'; 
+import Header from '../../components/Header';
+import * as Colors from '../../constants/Colors';
+import * as Fonts from '../../constants/Fonts';
 
-export default function MenuScreen() {
-  return (
-    <View>
-      <Text>Menu Screen</Text>
-    </View>
-  );
-}
+const MenuScreen = () => {
+    return (
+        <View style={styles.container}>
+            
+            <ImageBackground 
+                source={require('../../assets/images/cover.jpg')} 
+                style={styles.background}
+                resizeMode="cover"
+            >
+                <BlurView 
+                    intensity={10} 
+                    tint="dark" 
+                    style={StyleSheet.absoluteFill} 
+                />
+
+                <Header title="MENU" /> 
+
+            </ImageBackground>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: { 
+        flex: 1, 
+        backgroundColor: Colors.BACKGROUND_OVERLAY 
+    },
+    background: { 
+        flex: 1 
+    },
+    content: {
+        flex: 1,
+        alignItems: 'center',
+        paddingTop: 10,
+    },
+    placeholderText: {
+        color: Colors.TEXT_DARK,
+        fontSize: Fonts.SIZE_MENU_ITEM_NAME,
+        marginTop: 50,
+    }
+});
+
+export default MenuScreen;
