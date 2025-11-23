419dc190c7b8e82f369cd78b19d87c19c5023453
YO YO <suryoh764@github.com>
2025-11-23 18:01:09 +0700

    Penambahan Kategori

--- Yasaka/screens/user/MenuScreen.tsx
+++ Yasaka/screens/user/MenuScreen.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
-import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, ScrollView, TouchableOpacity } from 'react-native';
 import { BlurView } from 'expo-blur'; 
 import Header from '../../components/Header';
 import * as Colors from '../../constants/Colors';
 import * as Fonts from '../../constants/Fonts';
 
+const categories = ["PAKET", "AYAM", "MINUMAN", "KENTANG"];
+
 const MenuScreen = () => {
     return (
         <View style={styles.container}>
             
+            {/* Background + Blur */}
             <ImageBackground 
                 source={require('../../assets/images/cover.jpg')} 
                 style={styles.background}
                 resizeMode="cover"
             >
                 <BlurView 
-                    intensity={10} 
+                    intensity={30} 
                     tint="dark" 
                     style={StyleSheet.absoluteFill} 
                 />
 
+                {/* HEADER */}
                 <Header title="MENU" /> 
+
+                {/* CATEGORY BUTTONS */}
+                <View style={styles.categoryContainer}>
+                    {categories.map((cat, index) => (
+                        <TouchableOpacity key={index} style={styles.categoryButton}>
+                            <Text style={styles.categoryText}>{cat}</Text>
+                        </TouchableOpacity>
+                    ))}
+                </View>
 
             </ImageBackground>
         </View>
@@ -35,16 +48,27 @@ const styles = StyleSheet.create({
     background: { 
         flex: 1 
     },
-    content: {
-        flex: 1,
-        alignItems: 'center',
-        paddingTop: 10,
+
+    /* CATEGORY SECTION */
+    categoryContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop: 10,
+        gap: 10
     },
-    placeholderText: {
+
+    categoryButton: {
+        backgroundColor: Colors.PRIMARY,
+        paddingHorizontal: 15,
+        paddingVertical: 6,
+        borderRadius: 20,
+    },
+
+    categoryText: {
         color: Colors.TEXT_DARK,
-        fontSize: Fonts.SIZE_MENU_ITEM_NAME,
-        marginTop: 50,
-    }
+        fontSize: Fonts.SIZE_CATEGORY,
+        fontWeight: Fonts.WEIGHT_BOLD,
+    },
 });
 
 export default MenuScreen;
