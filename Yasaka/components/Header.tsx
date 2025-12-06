34eab1e1d25c36b958c3705c4a1fae9de142743d
YO YO <suryoh764@github.com>
2025-12-06 19:04:28 +0700

    penambahan prop onMenuPress dan TouchhableOpacity agar ikon humburger berfungsi

--- Yasaka/components/Header.tsx
+++ Yasaka/components/Header.tsx
@@ -1,29 +1,30 @@
 import React from 'react';
-import { View, Text, StyleSheet, Platform } from 'react-native';
-import { Ionicons } from '@expo/vector-icons'; 
+import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import * as Colors from '../constants/Colors';
 import * as Fonts from '../constants/Fonts';
 
 interface HeaderProps {
     title: string;
-    isTransparent?: boolean; 
+    isTransparent?: boolean;
+    onMenuPress?: () => void;
 }
 
-const Header: React.FC<HeaderProps> = ({ title, isTransparent = false }) => {
+const Header: React.FC<HeaderProps> = ({ title, isTransparent = false, onMenuPress }) => {
     return (
         <View style={[
-            styles.header, 
+            styles.header,
             isTransparent ? styles.headerTransparent : styles.headerDefault
         ]}>
-            <View style={styles.iconButton}> 
-                <Ionicons 
-                    name={'menu'} 
-                    size={30} 
-                    color={Colors.TEXT_DARK} 
+            <TouchableOpacity style={styles.iconButton} onPress={onMenuPress}>
+                <Ionicons
+                    name={'menu'}
+                    size={30}
+                    color={Colors.TEXT_DARK}
                 />
-            </View>
+            </TouchableOpacity>
             <Text style={styles.title}>{title}</Text>
-            <View style={styles.iconButtonPlaceholder} /> 
+            <View style={styles.iconButtonPlaceholder} />
         </View>
     );
 };
