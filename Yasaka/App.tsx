ae24bdeeeda16a97b4282f1bbe0a7566c4c361f4
YO YO <suryoh764@github.com>
2025-12-14 19:01:53 +0700

    mengganti DrawerNavigator dengan Stack.Navigator untuk meningkatkan struktur navigasi.

--- Yasaka/App.tsx
+++ Yasaka/App.tsx
@@ -1,18 +1,35 @@
-
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import DrawerNavigator from './navigation/DrawerNavigator';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { CartProvider } from './context/CartContext';
 import { OrderProvider } from './context/OrderContext';
+import DrawerNavigator from './navigation/DrawerNavigator';
+import AdminStack from './navigation/AdminStack';
+
+export type RootStackParamList = {
+  User: undefined;
+  Admin: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <CartProvider>
       <OrderProvider>
         <NavigationContainer>
-          <DrawerNavigator />
+          <Stack.Navigator
+            screenOptions={{ 
+              headerShown: false,
+              presentation: 'modal'
+            }}
+          >
+            <Stack.Screen name="User" component={DrawerNavigator} />
+            <Stack.Screen name="Admin" component={AdminStack} />
+          </Stack.Navigator>
         </NavigationContainer>
       </OrderProvider>
     </CartProvider>
   );
 }
+
