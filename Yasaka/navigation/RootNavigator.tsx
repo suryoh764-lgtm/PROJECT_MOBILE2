1400e30178051e1470ff8d585aa82cef4aa3c61d
YO YO <suryoh764@github.com>
2025-12-14 18:55:29 +0700

    menambahkan RootNavigator dengan navigasi ke DrawerNavigator dan fungsi untuk beralih ke mode pengguna

---
+++ Yasaka/navigation/RootNavigator.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import DrawerNavigator from './DrawerNavigator';
+
+
+export type RootStackParamList = {
+  User: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function RootNavigator() {
+  return (
+
+    <NavigationContainer>
+
+
+      <Stack.Navigator
+        screenOptions={{ 
+          headerShown: false,
+          presentation: 'modal'
+        }}
+      >
+        <Stack.Screen name="User" component={DrawerNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+// Function untuk switch ke user mode  
+export const switchToUserMode = (navigation: any) => {
+  navigation.reset({
+    index: 0,
+    routes: [{ name: 'User' }],
+  });
+};
+
