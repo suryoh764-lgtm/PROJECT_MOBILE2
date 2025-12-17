14029ee04ce32e6b437cb71adac6293e7e1d86c4
YO YO <suryoh764@github.com>
2025-12-18 00:49:27 +0700

    memperbaiki error navigasi setelah login admin dan mengintegrasikan drawer dengan stack navigator

--- Yasaka/navigation/AdminStack.tsx
+++ Yasaka/navigation/AdminStack.tsx
@@ -1,13 +1,18 @@
+
+
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import AdminLoginScreen from '../screens/admin/AdminLoginScreen';
 import TambahMenuScreen from '../screens/admin/TambahMenuScreen';
 import EditMenuScreen from '../screens/admin/EditMenuScreen';
 import EditStatusPesananScreen from '../screens/admin/EditStatusPesananScreen';
+import AdminDrawerContent from '../components/AdminDrawerContent';
 
 
 export type AdminStackParamList = {
     AdminLogin: undefined;
+    AdminDrawer: undefined;
     TambahMenu: undefined;
     EditMenu: { 
         menuId?: string;
@@ -18,6 +23,21 @@ export type AdminStackParamList = {
 
 const Stack = createNativeStackNavigator<AdminStackParamList>();
 
+const Drawer = createDrawerNavigator();
+
+const AdminDrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      screenOptions={{ headerShown: false }}
+      drawerContent={(props) => <AdminDrawerContent {...props} />}
+    >
+      <Drawer.Screen name="TambahMenu" component={TambahMenuScreen} />
+      <Drawer.Screen name="EditMenu" component={EditMenuScreen} />
+      <Drawer.Screen name="EditStatusPesanan" component={EditStatusPesananScreen} />
+    </Drawer.Navigator>
+  );
+};
+
 export default function AdminStack() {
   return (
     <Stack.Navigator
@@ -25,9 +45,7 @@ export default function AdminStack() {
       screenOptions={{ headerShown: false }}
     >
       <Stack.Screen name="AdminLogin" component={AdminLoginScreen} />
-      <Stack.Screen name="TambahMenu" component={TambahMenuScreen} />
-      <Stack.Screen name="EditMenu" component={EditMenuScreen} />
-      <Stack.Screen name="EditStatusPesanan" component={EditStatusPesananScreen} />
+      <Stack.Screen name="AdminDrawer" component={AdminDrawerNavigator} />
     </Stack.Navigator>
   );
 }
